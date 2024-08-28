3af9cefdfda41672aff71a84c00c5c866265f491
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-08-28 16:43:12 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
+// import searchImages from './js/pixabay-api.js';
+
+// document.getElementById('form').addEventListener('submit', function(event) {
+//     event.preventDefault(); 
+//     var inputValue = event.target.elements.input.value; 
+//     document.getElementById('main-ul').innerHTML = '';
+//     document.getElementById('load-more-button').style.display = 'none';
+
+//     if (inputValue.trim() === '') {
+//         alert('Please enter a search query.');
+//     } else {
+//         searchImages(inputValue);
+//     }
+//     event.target.reset();
+// });
+// main.js
 import searchImages from './js/pixabay-api.js';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault(); 
@@ -7,7 +25,10 @@ document.getElementById('form').addEventListener('submit', function(event) {
     document.getElementById('load-more-button').style.display = 'none';
 
     if (inputValue.trim() === '') {
-        alert('Please enter a search query.');
+        iziToast.warning({
+            message: 'Please enter a search query.',
+            position: 'topRight',
+        });
     } else {
         searchImages(inputValue);
     }

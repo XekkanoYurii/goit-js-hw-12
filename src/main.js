1124b71da9dce5f1e88e7880602963c5f01fce6a
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-08-28 16:48:45 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,4 @@
-// import searchImages from './js/pixabay-api.js';
 
-// document.getElementById('form').addEventListener('submit', function(event) {
-//     event.preventDefault(); 
-//     var inputValue = event.target.elements.input.value; 
-//     document.getElementById('main-ul').innerHTML = '';
-//     document.getElementById('load-more-button').style.display = 'none';
-
-//     if (inputValue.trim() === '') {
-//         alert('Please enter a search query.');
-//     } else {
-//         searchImages(inputValue);
-//     }
-//     event.target.reset();
-// });
-// main.js
 import searchImages from './js/pixabay-api.js';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";

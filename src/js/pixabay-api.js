ee82176ba53612461b9c81141753b998a0b36351
XekkanoYurii <150593149+XekkanoYurii@users.noreply.github.com>
2024-08-29 16:39:01 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,69 +1,74 @@
 import axios from 'axios';
 import renderImages from './render-functions.js';
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 export default searchImages;
-
-let page = 1; 
+let page = 1;
 let query = '';
-
+const loading = document.querySelector('.loader');
+loading.classList.add('hidden');
 async function searchImages(newQuery) {
-    if (newQuery !== query) {
-        query = newQuery;
-        page = 1;
+  if (newQuery !== query) {
+    query = newQuery;
+    page = 1;
+  }
+  let countForPage = 15;
+  const searchParams = new URLSearchParams({
+    key: '45503418-8e4034a5d9b1169325787ad22',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: countForPage,
+    page: page,
+  });
+  const url = `https://pixabay.com/api/?${searchParams}`;
+  try {
+    loading.classList.remove('hidden');
+    document.querySelector('span').style.display = 'block';
+    const response = await axios.get(url);
+    const data = response.data;
+    document.querySelector('span').style.display = 'none';
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        color: '#FF0000',
+        position: 'topRight',
+      });
+      return;
+    } else {
+      renderImages(data.hits);
     }
-
-    let countForPage = 15;
-
-    const searchParams = new URLSearchParams({
-        key: "45503418-8e4034a5d9b1169325787ad22",
-        q: query,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true,
-        per_page: countForPage,
-        page: page
-    });
-
-    const url = `https://pixabay.com/api/?${searchParams}`;
-    try {
-        document.querySelector('span').style.display = 'block'; 
-
-        const response = await axios.get(url); 
-        const data = response.data;
-
-        document.querySelector('span').style.display = 'none'; 
-
-        if (data.hits.length === 0) {
-            iziToast.error({
-                message: 'Sorry, there are no images matching your search query. Please try again!',
-                color: '#ff0000',
-                position: 'topRight',
-            });
-            return;
-        } else {
-            renderImages(data.hits);
-        }
-
-        if (countForPage * page >= data.totalHits) {
-            document.querySelector('#load-more-button').style.display = 'none';
-            iziToast.info({
-                message: "We're sorry, but you've reached the end of search results.",
-                position: 'topRight',
-            });
-        } else {
-            document.querySelector('#load-more-button').style.display = 'block';
-        }
-
-        page++;
-    } catch (error) {
-        console.error('Error:', error);
+    if (countForPage * page >= data.totalHits) {
+      document.querySelector('#load-more-button').style.display = 'none';
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+    } else {
+      document.querySelector('#load-more-button').style.display = 'block';
+      loading.classList.add('hidden');
     }
+    page++;
+    smoothScroll();
+  } catch (error) {
+    loading.classList.add('hidden');
+    console.error('Error:', error);
+  }
 }
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelector('#load-more-button').addEventListener('click', () => {
-        searchImages(query);
-    });
+document.addEventListener('DOMContentLoaded', event => {
+  document.querySelector('#load-more-button').addEventListener('click', () => {
+    searchImages(query);
+  });
 });
+function smoothScroll() {
+  const firstCard = document.querySelector('#parent-li');
+  if (firstCard) {
+    const cardHeight = firstCard.getBoundingClientRect().height;
+    window.scrollBy({
+      top: cardHeight * 2, //control
+      behavior: 'smooth',
+    });
+  }
+}
